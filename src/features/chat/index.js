658de4014b078a27973f0f7105fdfc20bada1715
Trandangtan12 <80817212+Trandangtan12@users.noreply.github.com>
2022-04-09 14:06:35 +0700

    test

--- src/features/chat/index.js
+++ src/features/chat/index.js
@@ -17,6 +17,7 @@ const Chat = () => {
   const [room, setRoom] = useState("");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
+  console.log("ok staging");
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
